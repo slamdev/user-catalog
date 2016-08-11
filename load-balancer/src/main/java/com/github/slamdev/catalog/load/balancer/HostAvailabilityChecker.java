d79983017823f0743b0617665c9a0cd3175f1e81
slamdev <slam.ul73@gmail.com>
2016-08-11 22:43:28 +0200

    code cleanup

--- load-balancer/src/main/java/com/github/slamdev/catalog/load/balancer/HostAvailabilityChecker.java
+++
@@ -1,6 +0,0 @@
-package com.github.slamdev.catalog.load.balancer;
-
-public interface HostAvailabilityChecker {
-
-    boolean isHostAvailable(String host);
-}
--- load-balancer/src/main/java/com/github/slamdev/catalog/load/balancer/LoadBalancedRequest.java
+++
@@ -1,8 +0,0 @@
-package com.github.slamdev.catalog.load.balancer;
-
-import java.io.IOException;
-
-public interface LoadBalancedRequest<T> {
-
-    T execute(String uri, String method) throws IOException;
-}
